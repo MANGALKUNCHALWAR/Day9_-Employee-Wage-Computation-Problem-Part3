cbed91c58c042910807307e2cb4bfa431588b47a
John Doe <mangalkunchalwar9999@gmail.com>
2022-12-23 10:23:04 +0530

    welcome to the Employee Wage Computation Problem Part3

---
+++ Main.java
@@ -0,0 +1,7 @@
+package assignmentday9.EmployeeWageComputationProblemPart3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to the Employee Wage Computation Problem Part3");
+    }
+}
